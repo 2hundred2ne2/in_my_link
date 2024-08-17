e0e60ac9078c6a2b118a8b69213ab66f7e560243
LMM07 <93696194+LMM07@users.noreply.github.com>
2024-08-18 00:31:49 +0900

    Update Input.tsx
    1. `shape` -> `variant` 수정
    2. `placeholder` 요소 삭제
    3. `w-[224px] h-[56px] p-[16px] rounded-[12px]` → `h-14 p-4 rounded-xl`
    4. `root --background-disabled: 228 228 231; /* zinc-200 */`
        `dark --background-disabled: 113 113 122; /* zinc-500 */`
    5. `const inputId = id ? id : useId();`
    6. `const classNames = ${variants[variant]} ${statuss[status]} ${className};`

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,16 +1,17 @@
 import React, { useId } from "react";
 
 interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
-  shape?: "default";
+  variant?: "default";
   status?: "default" | "error" | "disabled";
   type?: React.HTMLInputTypeAttribute;
   label?: string;
-  placeholder?: string;
   errorMessage?: string;
+  className?: string;
+  id?: string;
 }
 
-const shapes = {
-  default: "w-[224px] h-[56px] p-[16px] rounded-[12px] bg-background-muted",
+const variants = {
+  default: "h-14 p-4 rounded-xl bg-background-muted",
 };
 
 const statuss = {
@@ -21,23 +22,24 @@ const statuss = {
 };
 
 export function Input({
-  shape = "default",
+  variant = "default",
   status = "default",
   type,
   label,
-  placeholder,
   errorMessage,
+  id,
+  className,
   ...rest
 }: InputProps) {
-  const inputId = useId();
-  const className = `${shapes[shape]} ${statuss[status]}`;
+  const inputId = id ? id : useId();
+  const classNames = `${variants[variant]} ${statuss[status]} ${className}`;
 
   return (
     <div>
       <label htmlFor={inputId} className="block text-sm mb-2">
         {label}
       </label>
-      <input id={inputId} type={type} placeholder={placeholder} className={className} {...rest} />
+      <input id={inputId} type={type} className={classNames} {...rest} />
       {status === "error" && <p className="block text-danger text-sm">{errorMessage}</p>}
     </div>
   );
