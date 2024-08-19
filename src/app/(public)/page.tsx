16daf4fcd098536cdefdc118e1d6ebdf2ce2899c
jangmyeongji <2hundred2ne2@gmail.com>
2024-08-19 13:30:13 +0900

    feat:backdrop   사용하지 않는 코드 삭제

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,29 +1,3 @@
-"use client";
-import { useState } from "react";
-
-import { BackDrop } from "@/components/ui/backdrop";
-
 export default function HomePage() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <>
-      <main>hello</main>
-      <div>
-        <button onClick={() => setIsOpen(true)}>Open</button>
-        <button onClick={() => setIsOpen(false)}>Close</button>
-
-        {isOpen && (
-          <BackDrop>
-            <div className="p-4 bg-white rounded shadow-lg">
-              BackDrop 위
-              <button onClick={() => setIsOpen(false)} className="mt-4 text-red-500">
-                닫기
-              </button>
-            </div>
-          </BackDrop>
-        )}
-      </div>
-    </>
-  );
+  return <main>Home</main>;
 }
