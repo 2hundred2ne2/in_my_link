35cd0b4e8269c2c90929f724692e6f7e6916b713
chanwukim <chnwu.kim@gmail.com>
2024-08-20 12:54:06 +0900

    feat: AppHeader 컴포넌트 #26

---
+++ src/components/ui/app-header.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface AppHeaderProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function AppHeader({ children, className }: AppHeaderProps) {
+  return (
+    <header
+      className={cn(
+        "fixed top-0 bg-background grid grid-cols-[1fr_minmax(auto,65%)_1fr] h-16 w-full px-3 max-w-screen-sm",
+        className,
+      )}
+    >
+      {children}
+    </header>
+  );
+}
+
+export interface AppHeaderLeftProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function AppHeaderLeft({ children, className }: AppHeaderLeftProps) {
+  return <div className={cn("flex items-center", className)}>{children}</div>;
+}
+
+export interface AppHeaderCenterProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function AppHeaderCenter({ children, className }: AppHeaderCenterProps) {
+  return <div className={cn("flex items-center justify-center", className)}>{children}</div>;
+}
+
+export interface AppHeaderRightProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function AppHeaderRight({ children, className }: AppHeaderRightProps) {
+  return <div className={cn("flex items-center justify-end", className)}>{children}</div>;
+}
