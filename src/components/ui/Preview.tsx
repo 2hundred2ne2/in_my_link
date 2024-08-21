917e4b8ea28035c79494f81794cfc8a9bbc8740c
peanut <tqnls35100@gmail.com>
2024-08-21 13:24:34 +0900

    feat/ui/preview 퍼블리싱

---
+++ src/components/ui/Preview.tsx
@@ -0,0 +1,45 @@
+import { X } from "@phosphor-icons/react/dist/ssr";
+
+const Preview = () => {
+  return (
+    <>
+      <div className="mx-auto max-w-screen-sm min-h-dvh flex flex-col items-center">
+        <section className="flex flex-col items-center gap-3 my-8">
+          <button type="button" className="w-[95px] h-[95px] rounded-full bg-slate-600"></button>
+          <div className="flex flex-col items-center gap-3 my-2">
+            <div className="">닉네임</div>
+            <div className="">자신을 소개해볼까요?</div>
+          </div>
+        </section>
+        <section className="flex flex-col gap-5 pb-10 text-[15px]">
+          <div className="w-[336px] h-[70px] bg-white border border-border rounded-[16px] flex flex-row justify-start items-center gap-16 pl-6 cursor-pointer">
+            <div className="w-[45px] h-[45px] rounded-[16px] bg-background-muted"></div>
+            <div>instargram</div>
+          </div>
+          <div className="w-[336px] h-[70px] bg-white border border-border rounded-[16px] flex flex-row justify-start items-center gap-16 pl-6 cursor-pointer">
+            <div className="w-[45px] h-[45px] rounded-[16px] bg-background-muted"></div>
+            <div>kakao</div>
+          </div>
+          <div className="w-[336px] h-[70px] bg-white border border-border rounded-[16px] flex flex-row justify-start items-center gap-16 pl-6 cursor-pointer">
+            <div className="w-[45px] h-[45px] rounded-[16px] bg-background-muted"></div>
+            <div>blog</div>
+          </div>
+          <div className="w-[336px] h-[70px] bg-white border border-border rounded-[16px] flex flex-row justify-start items-center gap-16 pl-6 cursor-pointer">
+            <div className="w-[45px] h-[45px] rounded-[16px] bg-background-muted"></div>
+            <div>X</div>
+          </div>
+          <div className="w-[336px] h-[70px] bg-white border border-border rounded-[16px] flex flex-row justify-start items-center gap-16 pl-6 cursor-pointer">
+            <div className="w-[45px] h-[45px] rounded-[16px] bg-background-muted"></div>
+            <div>http://www.naver.com</div>
+          </div>
+        </section>
+        <section className="mb-10">
+          <div className="w-[50px] h-[50px] bg-white rounded-full shadow-lg flex justify-center items-center cursor-pointer hover:bg-background-muted">
+            <X size={22} weight="regular" color="#18181B" />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+export default Preview;
