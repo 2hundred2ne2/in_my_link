d29f61898301232d1d1ed8485a794d300135a113
LMM07 <mm_0028@naver.com>
2024-08-23 13:41:15 +0900

    test: 링크연결 아이콘 임시 추가

--- src/app/(public)/signup/link/page.tsx
+++ src/app/(public)/signup/link/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 
-import { Trash, Link as Chain } from "@phosphor-icons/react/dist/ssr";
+import {
+  Trash,
+  Link as Chain,
+  InstagramLogo,
+  FacebookLogo,
+  XLogo,
+  ThreadsLogo,
+} from "@phosphor-icons/react/dist/ssr";
 
 import {
   AppHeader,
@@ -34,7 +41,9 @@ export default function RegisterLinksPage() {
                     variant="muted"
                     className="rounded-2xl w-16 h-16 flex flex-col items-center"
                   >
-                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3"></span>
+                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3">
+                      <Chain size={40} />
+                    </span>
                   </Card>
                   <span>커스텀</span>
                 </button>
@@ -45,9 +54,9 @@ export default function RegisterLinksPage() {
                     variant="muted"
                     className="rounded-2xl w-16 h-16 flex flex-col items-center"
                   >
-                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3"></span>
+                    <InstagramLogo size={48} className="mt-2" />
                   </Card>
-                  <span>커스텀</span>
+                  <span>인스타그램</span>
                 </button>
               </li>
               <li>
@@ -56,9 +65,9 @@ export default function RegisterLinksPage() {
                     variant="muted"
                     className="rounded-2xl w-16 h-16 flex flex-col items-center"
                   >
-                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3"></span>
+                    <FacebookLogo size={48} className="mt-2" />
                   </Card>
-                  <span>커스텀</span>
+                  <span>페이스북</span>
                 </button>
               </li>
               <li>
@@ -67,9 +76,9 @@ export default function RegisterLinksPage() {
                     variant="muted"
                     className="rounded-2xl w-16 h-16 flex flex-col items-center"
                   >
-                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3"></span>
+                    <XLogo size={48} className="mt-2" />
                   </Card>
-                  <span>커스텀</span>
+                  <span>X</span>
                 </button>
               </li>
               <li>
@@ -78,19 +87,17 @@ export default function RegisterLinksPage() {
                     variant="muted"
                     className="rounded-2xl w-16 h-16 flex flex-col items-center"
                   >
-                    <span className="min-w-10 min-h-10 block bg-primary-300 rounded-xl mt-3"></span>
+                    <ThreadsLogo size={48} className="mt-2" />
                   </Card>
-                  <span>커스텀</span>
+                  <span>쓰레드</span>
                 </button>
               </li>
             </ul>
-            <ul className="flex flex-col gap-4 px-2 mt-10">
+            <ul className="flex flex-col gap-4 px-2 mt-20">
               <li>
                 <Card variant="default" className="rounded-2xl flex p-0 items-center">
-                  <span className="flex items-center w-8 h-8 mx-3 justify-center">
-                    <span className="flex items-center min-w-10 min-h-10 bg-primary-300 rounded-xl mt-0.5 p-1.5">
-                      <Chain size={28} />
-                    </span>
+                  <span className="flex items-center h-8 mx-3 justify-center">
+                    <InstagramLogo size={40} />
                   </span>
                   <input
                     type="text"
@@ -104,66 +111,15 @@ export default function RegisterLinksPage() {
               </li>
               <li>
                 <Card variant="default" className="rounded-2xl flex p-0 items-center">
-                  <span className="flex items-center w-8 h-8 mx-3 justify-center">
-                    <span className="flex items-center min-w-10 min-h-10 bg-primary-300 rounded-xl mt-0.5 p-1.5">
+                  <span className="flex items-center w-8 h-8 mx-4 justify-center">
+                    <span className="flex items-center bg-primary-300 rounded-xl mt-0.5 p-1.5">
                       <Chain size={28} />
                     </span>
                   </span>
                   <input
                     type="text"
                     className="flex-auto items-center px-3 py-4 w-full h-7 mt-4 mb-4"
-                    placeholder="https://instagram.com/"
-                  ></input>
-                  <button className="flex-none items-center w-7 h-7 m-2 justify-center">
-                    <Trash size={18} />
-                  </button>
-                </Card>
-              </li>
-              <li>
-                <Card variant="default" className="rounded-2xl flex p-0 items-center">
-                  <span className="flex items-center w-8 h-8 mx-3 justify-center">
-                    <span className="flex items-center min-w-10 min-h-10 bg-primary-300 rounded-xl mt-0.5 p-1.5">
-                      <Chain size={28} />
-                    </span>
-                  </span>
-                  <input
-                    type="text"
-                    className="flex-auto items-center px-3 py-4 w-full h-7 mt-4 mb-4"
-                    placeholder="https://instagram.com/"
-                  ></input>
-                  <button className="flex-none items-center w-7 h-7 m-2 justify-center">
-                    <Trash size={18} />
-                  </button>
-                </Card>
-              </li>
-              <li>
-                <Card variant="default" className="rounded-2xl flex p-0 items-center">
-                  <span className="flex items-center w-8 h-8 mx-3 justify-center">
-                    <span className="flex items-center min-w-10 min-h-10 bg-primary-300 rounded-xl mt-0.5 p-1.5">
-                      <Chain size={28} />
-                    </span>
-                  </span>
-                  <input
-                    type="text"
-                    className="flex-auto items-center px-3 py-4 w-full h-7 mt-4 mb-4"
-                    placeholder="https://instagram.com/"
-                  ></input>
-                  <button className="flex-none items-center w-7 h-7 m-2 justify-center">
-                    <Trash size={18} />
-                  </button>
-                </Card>
-              </li>
-              <li>
-                <Card variant="default" className="rounded-2xl flex p-0 items-center">
-                  <span className="flex items-center w-8 h-8 mx-3 justify-center">
-                    <span className="flex items-center min-w-10 min-h-10 bg-primary-300 rounded-xl mt-0.5 p-1.5">
-                      <Chain size={28} />
-                    </span>
-                  </span>
-                  <input
-                    type="text"
-                    className="flex-auto items-center px-3 py-4 w-full h-7 mt-4 mb-4"
-                    placeholder="https://instagram.com/"
+                    placeholder="URL을 입력해주세요"
                   ></input>
                   <button className="flex-none items-center w-7 h-7 m-2 justify-center">
                     <Trash size={18} />
