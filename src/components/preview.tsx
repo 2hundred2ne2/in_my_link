cc351f3de2bba33eff8f6839ade74d56e1224c89
peanut <tqnls35100@gmail.com>
2024-08-23 14:20:31 +0900

    z-index 수정

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,7 +1,7 @@
 export function Preview() {
   return (
     <>
-      <div className="w-[640px] h-[700px] bg-pink-300 flex flex-col items-center justify-center rounded-xl z-20">
+      <div className="w-[640px] h-[700px] bg-pink-300 flex flex-col items-center justify-center rounded-xl z-9">
         미리보기 화면입니다.
       </div>
     </>
