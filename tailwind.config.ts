8b121854a64364594dce5d4325af43aa0e5f235a
peanut <tqnls35100@gmail.com>
2024-08-25 13:56:00 +0900

    animate 위치 조정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,12 +30,12 @@ const config: Config = {
       },
       keyframes: {
         slideUp: {
-          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "0%": { transform: "translateY(10%)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
         },
         slideDown: {
           "0%": { transform: "translateY(0)", opacity: "1" },
-          "100%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(10%)", opacity: "0" },
         },
       },
       animation: {
