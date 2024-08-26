51651256462dd3e2ab9d5894284f65045671b71f
chanwukim <chnwu.kim@gmail.com>
2024-08-26 14:31:46 +0900

    chore: mysql 기본 설정 #57

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+
+/**
+ * @see https://sidorares.github.io/node-mysql2/docs#using-connection-pools
+ */
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+});
