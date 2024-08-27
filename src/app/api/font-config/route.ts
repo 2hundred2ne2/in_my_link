2534ec3c6ad76201630f04f1ccb23efa4b4f1c18
chanwukim <chnwu.kim@gmail.com>
2024-08-27 14:51:00 +0900

    feat: 폰트 설정 API #82

--- src/app/api/font-config/route.ts
+++ src/app/api/font-config/route.ts
@@ -24,3 +24,21 @@ export async function GET() {
 
   return NextResponse.json(fontConfigs[0], { status: 200 });
 }
+
+/**
+ * @see https://github.com/2hundred2ne2/in_my_link/issues/82
+ */
+export async function PATCH(request: Request) {
+  // TODO: 권한
+  const TEMP_USER_ID = 1;
+
+  const body = await request.json();
+  const { size, color, type } = body;
+
+  await db.query(
+    "UPDATE font_config SET size = ?, color = ?, type = ?, update_date = NOW() WHERE user_id = ?",
+    [size, color, type, TEMP_USER_ID],
+  );
+
+  return NextResponse.json({ message: "폰트 설정이 업데이트 되었습니다" }, { status: 200 });
+}
