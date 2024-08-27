ca0a95f3b60041e4185cd99c694ff9db2b66619a
chanwukim <chnwu.kim@gmail.com>
2024-08-27 16:32:58 +0900

    refactor: LinkEditor 경로 변경 #37

--- src/components/links/link-editor.tsx
+++ src/components/links/link-editor.tsx
@@ -31,11 +31,11 @@ import {
 
 import { cn } from "@/lib/utils";
 
-import { Button } from "./ui/button";
-import { Card } from "./ui/card";
-import { Heading } from "./ui/heading";
-import { Modal } from "./ui/modal";
-import { Text } from "./ui/text";
+import { Button } from "../ui/button";
+import { Card } from "../ui/card";
+import { Heading } from "../ui/heading";
+import { Modal } from "../ui/modal";
+import { Text } from "../ui/text";
 
 /**
  * TODO: 코드 길어지면 파일로 분리
