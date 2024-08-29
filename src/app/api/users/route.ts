8acd294291183de417752da0a238fdfbd593b2a6
jangmyeongji <2hundred2ne2@gmail.com>
2024-08-29 19:54:56 +0900

    feat/68    사용자 삭제

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -92,3 +92,32 @@ export async function PATCH(req: Request) {
     }
   }
 }
+// 사용자 삭제 API
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+
+  if (!id) {
+    return NextResponse.json({ message: "사용자 ID를 제공해 주세요" }, { status: 400 });
+  }
+
+  try {
+    // 사용자 삭제 쿼리
+    const deleteQuery = "DELETE FROM user WHERE id = ?";
+    const [result] = await db.execute<ResultSetHeader>(deleteQuery, [id]);
+
+    if (result.affectedRows === 0) {
+      return NextResponse.json({ message: "사용자 삭제 실패" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "사용자가 성공적으로 삭제되었습니다." }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: "사용자를 삭제하는 중 오류가 발생했습니다", error: error.message },
+        { status: 500 },
+      );
+    } else {
+      return NextResponse.json({ message: "알 수 없는 오류 발생" }, { status: 500 });
+    }
+  }
+}
