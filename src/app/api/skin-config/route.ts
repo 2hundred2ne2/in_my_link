f48ebd78364d3d43b5d22527f34d49d8d4b1910b
peanut <tqnls35100@gmail.com>
2024-09-11 14:40:57 +0900

    bgImage api 수정

--- src/app/api/skin-config/route.ts
+++ src/app/api/skin-config/route.ts
@@ -33,7 +33,7 @@ export async function GET(request: NextRequest) {
   }
 
   const [skinConfigs] = await db.query<SkinConfig[]>(
-    "SELECT color FROM skin_config WHERE user_id = ?",
+    "SELECT color, bgImage FROM skin_config WHERE user_id = ?",
     [users[0].id],
   );
 
@@ -41,19 +41,33 @@ export async function GET(request: NextRequest) {
 }
 
 /**
- * @see https://github.com/2hundred2ne2/in_my_link/issues/84
- */
+ 
+* @see https://github.com/2hundred2ne2/in_my_link/issues/84
+*/
 export async function PATCH(request: Request) {
-  // TODO: 권한
-  const TEMP_USER_ID = 1;
+  try {
+    // TODO: 권한 확인
+    const TEMP_USER_ID = 1;
 
-  const body = await request.json();
-  const { color } = body;
+    const body = await request.json();
+    const { color, bgImage } = body;
 
-  await db.query("UPDATE skin_config SET color = ?, update_date = NOW() WHERE user_id = ?", [
-    color,
-    TEMP_USER_ID,
-  ]);
+    if (color) {
+      await db.query("UPDATE skin_config SET color = ?, update_date = NOW() WHERE user_id = ?", [
+        color,
+        TEMP_USER_ID,
+      ]);
+    }
+    if (bgImage) {
+      await db.query("UPDATE skin_config SET bgImage = ?, update_date = NOW() WHERE user_id = ?", [
+        bgImage,
+        TEMP_USER_ID,
+      ]);
+    }
 
-  return NextResponse.json({ message: "스킨 설정이 업데이트 되었습니다" }, { status: 200 });
+    return NextResponse.json({ message: "스킨 설정이 업데이트 되었습니다" }, { status: 200 });
+  } catch (error) {
+    console.error("[PATCH] /api/skin-config: \n", error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
